8b72790346adcdbe7252011e1e34db2f05fb9e8a
Daniel18lol <47545494+Daniel18lol@users.noreply.github.com>
2020-10-20 00:18:15 -0500

    Componentes Final
    Se terminan los componentes con la navegación y funcionalidades. Falta estilizar los componentes para terminar

---
+++ frontend/src/components/Contactenos.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {useHistory} from 'react-router-dom'
+import {useForm} from 'react-hook-form';
+
+export default function Contactenos(){
+    const {register,handleSubmit,errors} = useForm();
+    const history = useHistory();
+
+    const onSubmit = (data,e) => {
+        window.open(`mailto:carlos.fernandezg@upb.edu.co?subject=Peticion FamiAhorro&body=${data.pqr}`)
+        history.push('/')
+    }
+
+    return(
+            <form className='container' onSubmit={handleSubmit(onSubmit)}>
+                <div className="row">
+                    <h2 className='text-primary col-12 mb-5 mt-5'>Contáctenos</h2>
+                    <div className="form-group col-12">
+                        <label className='text-primary mb-5' htmlFor="email">Correo Electrónico</label>
+                        <input type='text' className='form-control input mb-5 w-50 mx-auto' placeholder='Ingrese su correo' name="email" defaultValue="" ref={register({required:true})} />
+                        {errors.email && <span className="text-danger">¡Ingrese su correo de contacto!</span>}
+                    </div>
+                    <div className="form-group col-12">
+                        <textarea name="pqr" placeholder='Escriba su solicitud o comentario' cols='40' rows='7' id="pqr" defaultValue="" ref={register({required:true})}></textarea>
+                        {errors.pqr && <span className="text-danger"><br/>Por favor ingrese su comentario</span>}
+                    </div>
+                    <div className="form-group col-12">
+                    <button type='submit' className='btn btn-primary'>Contactar</button>
+                </div>
+                </div>
+            </form>
+    )
+}
---
+++ frontend/src/components/QuienesSomos.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import CarlosImg from '../img/carlos.jpg'
+import GianImg from '../img/gian.jpg'
+
+export default function QuienesSomos(){
+    return (
+        <main className='container'>
+            <div className="row">
+                <div className="col-12">
+                    <h2 className='text-primary mt-5 mb-5'>¿Quiénes somos?</h2>
+                </div>
+                <div class="card-deck justify-content-center">
+                    <div class="card border-primary align-items-center mr-4">
+                        <div class="card-body">
+                        <h5 class="card-header border-primary mb-5">Carlos Fernández</h5>
+                        <img class="card-img-top w-50 h-50 mb-5 rounded-circle" src={CarlosImg} alt="Carlos Fernández"/>
+                        <p class="card-text">Estudiante Ingeniería de Sistemas e Informática. <br/>
+                        Octavo Semestre</p>
+                        </div>
+                        
+                    </div>
+                    <div class="card border-primary align-items-center">
+                        
+                        <div class="card-body">
+                        <h5 class="card-header border-primary mb-5">Gian Paul Moreno</h5>
+                        <img class="card-img-top w-50 h-50 mb-5 rounded-circle" src={GianImg} alt="Gian Paul Moreno"/>
+                        <p class="card-text">Estudiante Ingeniería de Sistemas e Informática. <br/>
+                        Octavo Semestre</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        )
+}
